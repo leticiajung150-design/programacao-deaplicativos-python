eea678422b32c253681c87c90cd6b954ed305038
Letícia Jung <leticia_jung150@estudante.sesisenai.org.br>
2026-08-31 09:19:46 -0300

    Adiciona exemplo de operador lógico and

---
+++ 02_operadores_logicos_estruturas_condicionais/exemplo.py
@@ -0,0 +1,13 @@
+# AULA: OPERADORES LÓGICOS E ESTRUTURAS CONDICIONAIS
+# 1. OPERADORES LÓGICOS
+
+# Os operadores lógicos permitem combinar condições.
+
+#and
+#Toas as condições precisam ser verdadeiras.
+
+idade = 20
+possui_carteira = True
+
+resultado = idade >= 18 and possui_carteira
+print(resultado)
